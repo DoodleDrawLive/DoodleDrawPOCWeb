abea6ae47369776e314238860307d17ef9f66be2
ptitarchuk <p.titarchuk@gmail.com>
2024-03-15 14:29:20 +0200

    Update web version

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-DoodleDrawPOC-1.0";
+const cacheName = "DefaultCompany-DoodleDrawPOC-1.2";
 const contentToCache = [
     "Build/DoodleDrawPOCWeb.loader.js",
     "Build/DoodleDrawPOCWeb.framework.js.unityweb",
